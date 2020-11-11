753b95d177f5bd083fe8c7f43b6af02550fcb02c
Павел Павел <Zarrderr@yandex.ru>
2020-11-11 13:20:15 +0500

    Добавьте файлы проекта.

---
+++ Bot.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BotMax
+{
+    class Bot
+    {
+        public string Name;
+
+        bool isGreeted = false;
+
+
+        public void Greetings() 
+        {
+            Console.WriteLine("привет" + Name + "!");
+            Console.WriteLine("о чем задумался приятель?");
+
+            bool isGreeted = true;
+
+        }
+
+
+        public void Sayit()
+        {
+            if (isGreeted)
+            {
+                Console.WriteLine("я уже здоровался");
+
+            }
+            else
+            {
+                Console.WriteLine("я еще не здоровался");
+            }
+        }
+    }
+}
+ 
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BotMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bot Max = new Bot();
+            Max.Name = "MAX";
+            Max.Greetings();
+            Max.Sayit();
+        }
+    }
+}

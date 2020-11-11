3099f7c54381ad9934547737c9034e3f4b5e152e
Павел Павел <Zarrderr@yandex.ru>
2020-11-11 16:48:21 +0500

    Пошел процес создания проги

--- Bot.cs
+++ Bot.cs
@@ -33,6 +33,42 @@ namespace BotMax
                 Console.WriteLine("я еще не здоровался");
             }
         }
+
+
+        public void CountToTen()
+        {
+            
+            
+            Console.WriteLine("считаю до десяти!");
+            Console.WriteLine("1");
+            Console.WriteLine("2");
+            Console.WriteLine("3");
+            Console.WriteLine("4");
+            Console.WriteLine("5");
+            Console.WriteLine("6");
+            Console.WriteLine("7");
+
+
+
+        }
+
+
+        public void CountwhithWhile()
+        {
+
+
+            Console.WriteLine("считаю до десяти с помощью цикла!");
+            int number = 1;
+
+        while (number <= 10)
+            {
+
+                Console.WriteLine(number + "!");
+
+
+            }
+        }
+
     }
 }
  
--- Program.cs
+++ Program.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 
-namespace BotMax
+namespace BotMax     //имя проэкта
 {
-    class Program
+    class Program    // основной класс программы
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // основной метод программы (точка входа)  в нем буду вызывать остольные методы
         {
             Bot Max = new Bot();
-            Max.Name = "MAX";
-            Max.Greetings();
-            Max.Sayit();
+            Max.Name = "MAX";    
+            Max.Greetings();   // метод привет о чем задумался
+            Max.Sayit();       // метод здаровался или нет
+            Max.CountToTen();  // метод счет до 10 последовательно
+            Max.CountwhithWhile(); // метод счет до 10 циклом вайл
         }
     }
 }
